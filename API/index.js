a72ef9f222d1fac228cb31cc157c688cad8bdd6a
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-14 18:32:50 +0700

    update index.js

--- API/index.js
+++ API/index.js
@@ -10,14 +10,23 @@ router.get('/autogempa', async (req, res) => {
   try {
     const response = await axios.get('https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json');
     const data = response.data;
-    if (data) {
+
+    if (data && data.Infogempa && data.Infogempa.gempa) {
+      data.Infogempa.gempa.Shakemap = "https://data.bmkg.go.id/DataMKG/TEWS/" + data.Infogempa.gempa.Shakemap;
       res.succesJson(data.Infogempa.gempa);
     } else {
       res.status(404).errorJson({ message: 'Data tidak ditemukan atau format tidak valid.' });
     }
   } catch (error) {
     console.error('Terjadi kesalahan saat mengambil data autogempa:', error);
-    res.status(500).errorJson({ message: 'Terjadi kesalahan saat mengambil data autogempa.' });
+
+    if (error.response) {
+      res.status(error.response.status).errorJson({ message: `Kesalahan server: ${error.response.statusText}` });
+    } else if (error.request) {
+      res.status(500).errorJson({ message: 'Tidak dapat terhubung ke server BMKG.' });
+    } else {
+      res.status(500).errorJson({ message: 'Terjadi kesalahan saat mengambil data autogempa.' });
+    }
   }
 });
 
