56ff808d7bbbd4d737009c505ab01ef621264788
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-15 13:43:04 +0700

    update

--- API/index.js
+++ API/index.js
@@ -8,6 +8,28 @@ const { jadwal } = require('../lib/animeJadwal.js');
 const crypto = require('crypto');
 const alicia = require('../lib/alicia.js');
 
+router.get('/simsimi', async (req, res) => {
+  const msg = req.query.msg;
+  const id = req.query.lang || "id";
+
+  if (!msg) {
+    return res.errorJson({ error: "Parameter 'msg' harus disediakan." });
+  }
+
+  try {
+    const response = await axios.post('https://simsimi.vn/web/simtalk', `text=${encodeURIComponent(msg)}&lc=${id}`, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    res.succesJson({ respon: response.data.success });
+  } catch (error) {
+    console.error("Error calling SimSimi API:", error);
+    res.errorJson({ error: "Terjadi kesalahan saat memproses permintaan SimSimi." });
+  }
+});
+
 router.get('/alicia', async (req, res) => {
   const user = req.query.user;
   const msg = req.query.msg;
