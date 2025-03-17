ad66d046d8eecef0f513f007e50c09da9981c86e
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-18 04:30:31 +0700

    update

--- API/index.js
+++ API/index.js
@@ -10,6 +10,25 @@ const alicia = require('../lib/alicia.js');
 const gq = require('../lib/genrateQuery.js');
 const snapsave = require('../lib/snapsave.js')
 
+router.get('/artinama', async (req, res) => {
+  const nama = req.query.nama;
+
+  if (!nama) {
+    return res.errorJson({ error: "Parameter 'nama' harus disediakan." });
+  }
+
+  try {
+    const apiUrl = `https://express-vercel-ytdl.vercel.app/arti?nama=${encodeURIComponent(nama)}`;
+    const response = await axios.get(apiUrl, { responseType: 'stream' });
+
+    res.setHeader('Content-Type', 'image/jpeg');
+    response.data.pipe(res);
+  } catch (error) {
+    console.error("Error fetching or streaming khodam image:", error);
+    res.errorJson({ error: "Gagal mengambil atau mengirim gambar khodam." });
+  }
+});
+
 router.get('/khodam', async (req, res) => {
   const nama = req.query.nama;
 
