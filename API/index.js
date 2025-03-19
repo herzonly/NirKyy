428eb45d6050a503c147e6f63912dd5ffdc6e377
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-20 06:55:41 +0700

    update

--- API/index.js
+++ API/index.js
@@ -10,6 +10,25 @@ const alicia = require('../lib/alicia.js');
 const gq = require('../lib/genrateQuery.js');
 const snapsave = require('../lib/snapsave.js')
 
+router.get('/brats', async (req, res) => {
+  const host = req.query.host;
+  const text = req.query.text;
+
+  if (!host || !text) {
+    return res.errorJson({ error: "Parameter 'host' dan 'text' harus disediakan." });
+  }
+
+  try {
+    const apiUrl = `https://express-vercel-ytdl.vercel.app/brats?host=${encodeURIComponent(host)}&text=${encodeURIComponent(text)}`;
+    const response = await axios.get(apiUrl, { responseType: 'stream' });
+    res.setHeader('Content-Type', 'image/png');
+    response.data.pipe(res);
+  } catch (error) {
+    console.error("Error fetching or streaming brats image:", error);
+    res.errorJson({ error: "Gagal mengambil atau mengirim gambar brats." });
+  }
+});
+
 router.get('/artinama', async (req, res) => {
   const nama = req.query.nama;
 
