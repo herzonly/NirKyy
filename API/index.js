d0b7f95c92290557a6890fd6590863428d0e0090
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-20 14:13:23 +0700

    update

--- API/index.js
+++ API/index.js
@@ -11,7 +11,7 @@ const gq = require('../lib/genrateQuery.js');
 const snapsave = require('../lib/snapsave.js')
 const speechma = require('../lib/speechma.js')
 
-router.get('/tes', speechma);
+router.get('/speechma', speechma);
 router.get('/brats', async (req, res) => {
   const host = req.query.host;
   const text = req.query.text;
