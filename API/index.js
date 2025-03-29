7627dbf31083fff806cb0ee5f249b2a90da1458a
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-29 09:06:51 +0700

    update

--- API/index.js
+++ API/index.js
@@ -32,6 +32,33 @@ router.get('/removebg', rmbg)
 router.get('/luminai', luminai)
 router.get('/gemini',gemini)
 
+router.get('/wikipedia-random', async (req, res) => {
+    try {
+        const url = 'https://id.m.wikipedia.org/wiki/Special:Random';
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+        
+        const title = $('span.mw-page-title-main').text().trim() || "";
+        const description = $('p')
+            .map((i, el) => $(el).text().trim().replace(/\[\d+\]/g, ''))
+            .get()
+            .filter(text => text.length > 0)
+            .join('\n\n') || "";
+        
+        const images = $('figure.mw-default-size img')
+            .map((i, el) => `https:${$(el).attr('src')}`)
+            .get();
+        
+        res.json({
+            title,
+            description,
+            images
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Gagal mengambil data' });
+    }
+});
+
 router.get('/anime-popular', async (req, res) => {
   try {
     const response = await axios.get('https://myanimelist.net/topanime.php?type=bypopularity', {
