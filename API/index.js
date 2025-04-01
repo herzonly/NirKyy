cd9dc783d1b4c422af3352539b66c40973952ed2
Rikikangsc2-eng <nueapi11@gmail.com>
2025-04-01 21:18:20 +0700

    add tiktok

--- API/index.js
+++ API/index.js
@@ -21,7 +21,9 @@ const chatgpt = require('../lib/chatgpt.js')
 const translate = require('../lib/translate.js');
 const yanzGPT = require('../lib/yanzGPT.js')
 const spotify = require('../lib/spotify.js')
+const tiktokScrapt = require('../lib/tiktok.js')
 
+router.get('/tiktokdl', tiktokScrapt)
 router.get('/spotifydl', spotify);
 router.get('/yanzgpt', yanzGPT);
 router.get('/gemini-translate', translate)
