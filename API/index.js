83d7ec2b3b5fbbce9c58018ddf943cadeaa06f26
Rikikangsc2-eng <nueapi11@gmail.com>
2025-05-10 15:56:20 +0700

    TES SS MANCHINE

--- API/index.js
+++ API/index.js
@@ -33,6 +33,7 @@ const fluxf = require('../lib/fluxFast.js')
 const ytaudiov2 = require('../lib/ytAudioV2.js')
 const komiku = require('../lib/komiku.js')
 //-
+router.get('/ssweb',require('../lib/ssmanchine.js'))
 router.get('/tictactoe',require('../lib/tictactoe.js'))
 router.get('/image-random',require('../lib/image-random.js'))
 router.get('/komik',komiku)
