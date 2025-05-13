7fad6a702cf9b984bff6bada11244b9235d3710a
Rikikangsc2-eng <nueapi11@gmail.com>
2025-05-13 18:35:28 +0700

    update

--- API/index.js
+++ API/index.js
@@ -59,6 +59,36 @@ router.get('/removebg', rmbg)
 router.get('/luminai', luminai)
 router.get('/gemini',gemini)
 
+router.get('/soundmeme',async (req,res) =>{
+    try {
+    const targetUrl = `https://www.myinstants.com/en/index/id?page=${encodeURIComponent(req.query.page||'1')}`
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.7103.60 Mobile Safari/537.36',
+      'Referer': 'https://www.myinstants.com/en/index/id/'
+    }
+    const response = await axios.get(targetUrl, { headers })
+    const $ = cheerio.load(response.data)
+    const instants = []
+    $('.instants.result-page .instant').each((i, el) => {
+      const title = $(el).find('.instant-link').text().trim()
+      const onclick = $(el).find('.small-button').attr('onclick')
+      let audio = null
+      if (onclick) {
+        const match = onclick.match(/play\('([^']+)'/)
+        if (match && match[1]) {
+          audio = 'https://www.myinstants.com' + match[1]
+        }
+      }
+      if (title && audio) {
+        instants.push({ title, audio })
+      }
+    })
+    res.successJson({ data: instants })
+  } catch (e) {
+    res.errorJson('Yah, gagal nih ngambil datanya!', 500)
+  }
+});
 router.get('/tiktok-search', async (req,res) => {
   try {
     const query = req.query.query
